d1b32b428964cd74f474c46dd5d5ef6271030d2e
jve <joe.veyhl@gmail.com>
2022-11-04 23:35:32 -0400

    added eventlistener for user selecting answer choices

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,11 +1,11 @@
 // Get reference to h2 that holds the question
 const question = document.querySelector("#question");
-// Get reference to p that hold the answer choices and make it an array
+// Get reference to p that holds the answer choices and make it an array
 const choices = Array.from(document.querySelectorAll(".choice-text"));
 
 let currentQuestion = {};
 // for creating a delay between questions
-let acceptingAnswers = true;
+let canAnswer = false;
 let score = 0;
 let questionCounter = 0;
 // five quiz question objects in an array
@@ -62,13 +62,39 @@ const getQuestion = () => {
     let chosenQuestion = questions[questionIndex];
     // set the question in the h2
     question.innerText = chosenQuestion.question; 
-}
+    // get the answer options from the questions array; itterate through each of the choices and get a reference to each choice
+    choices.forEach( choice => {
+        // get the number from the data-number property in the p that holds the answer choice
+        const number = choice.dataset["number"];
+        // concatenate the string "choice" with the data set number to create an index to access the answer choices in the current question
+        choice.innerText = chosenQuestion["choice"+number];
+    });
+
+    // allow user to answer
+    canAnswer = true;
+};
 
 // initialize a new game
 const startGame = () => {
     // set score to zero
     score = 0;
     getQuestion();
-}
+};
+
+choices.forEach( choice => {
+    choice.addEventListener("click", e => {
+        // ignore clicks until we decide they can answer
+        if(!canAnswer) return;
+
+        // make delay between answers
+        canAnswer = false;
+
+        // store the user's choice
+        const selectedChoice = e.target;
+        // get the data-number associated with the user's choice
+        const selectedAnswer = selectedChoice.dataset["number"];
+        //LEFT OFF HERE
+    });
+});
 
 startGame();
